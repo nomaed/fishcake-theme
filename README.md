57bfb3f1a14c4100561d040cca076987bd478582
Boris Aranovich <nomaed@gmail.com>
2016-06-19 23:24:52 +0300

    Initial commit, forking theme off fishcake package

---
+++ README.md
@@ -0,0 +1,33 @@
+<img src="https://raw.githubusercontent.com/nomaed/fishcake/master/fishcake.png" align="left" width="200px"/>
+
+#### Fishcake theme
+> A theme for [Oh My Fish][omf-link].
+
+[![MIT License](https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square)](/LICENSE)
+[![Fish Shell Version](https://img.shields.io/badge/fish-v2.2.0-007EC7.svg?style=flat-square)](https://fishshell.com)
+[![Oh My Fish Framework](https://img.shields.io/badge/Oh%20My%20Fish-Framework-007EC7.svg?style=flat-square)](https://www.github.com/oh-my-fish/oh-my-fish)
+
+<br/>
+
+
+## Install
+
+### Prerequisite
+
+Make sure that [Fishcake package](http://github.com/nomaed/fishcake) is installed before using the theme.
+
+```fish
+$ omf install fishcake-theme
+```
+
+# License
+
+[MIT][mit] © [{{USER}}][author] et [al][contributors]
+
+
+[mit]:            https://opensource.org/licenses/MIT
+[author]:         https://github.com/nomaed
+[contributors]:   https://github.com/nomaed/fishcake-theme/graphs/contributors
+[omf-link]:       https://www.github.com/oh-my-fish/oh-my-fish
+
+[license-badge]:  https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square
---
+++ fish_prompt.fish
@@ -0,0 +1,80 @@
+function fish_prompt
+  # Cache exit status
+  set -l last_status $status
+
+  # Just calculate these once, to save a few cycles when displaying the prompt
+  if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
+  if not set -q __fish_prompt_char
+    switch (id -u)
+      case 0
+		set -g __fish_prompt_char '#'
+      case '*'
+  		set -g __fish_prompt_char '▶'
+    end
+  end
+
+  set -l normal (set_color normal)
+  set -l white (set_color FFFFFF)
+  set -l red (set_color FF0000)
+  set -l grey (set_color 999999)
+  set -l turquoise (set_color 5fdfff)
+  set -l orange (set_color df5f00)
+  set -l hotpink (set_color df005f)
+  set -l limegreen (set_color 87ff00)
+  set -l purple (set_color af5fff)
+  
+  # set stuff according to last exit code
+  set -l _prompt_prefix_color
+  set -l _prompt_char
+
+  if test $last_status -eq 0
+	set _prompt_prefix_color $white
+	set _prompt_char $__fish_prompt_char
+  else
+  	set _prompt_prefix_color $red
+  	set _prompt_char '⛔ '
+  end
+
+  # Configure __fish_git_prompt
+  set -g __fish_git_prompt_char_stateseparator ' '
+  set -g __fish_git_prompt_color 5fdfff
+  set -g __fish_git_prompt_color_flags df5f00
+  set -g __fish_git_prompt_color_prefix white
+  set -g __fish_git_prompt_color_suffix white
+  set -g __fish_git_prompt_showdirtystate true
+  set -g __fish_git_prompt_showuntrackedfiles true
+  set -g __fish_git_prompt_showstashstate true
+  set -g __fish_git_prompt_show_informative_status true
+
+  # Line 1
+  set -l prefix "$_prompt_prefix_color╭─$hotpink$USER$grey"
+  if set -q SSH_TTY
+    set prefix "$prefix@$orange$__fish_prompt_hostname$grey"
+  end
+  set prefix "$prefix:$limegreen"
+  set -l pwd (prompt_pwd)
+  set -l suffix (__fish_git_prompt ' (%s)')
+  set -l clock_and_status (get_clock_and_status $last_status)
+  # set -l lengths (string length (strip_codes "$prefix$pwd$suffix"))
+  echo -s $prefix $pwd $suffix $clock_and_status
+  # [$lengths / $COLUMNS]"
+
+  # Line 2
+  echo -n -s $_prompt_prefix_color '╰' $_prompt_char $normal ' '
+end
+
+function get_clock_and_status
+  echo ' '
+  if set -q argv; and test $argv[1] -ne 0
+    set_color red; echo ''
+    set_color -bred black; echo $argv[1]; set_color normal
+    set_color red; echo ' '
+    set_color normal
+  end
+  echo '⌚ '
+  set_color 666
+  date "+%H:%M:%S"
+  set_color normal
+end
