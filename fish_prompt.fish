e91c0de8a617fa263806a36245fec0d79a416f71
Boris Aranovich <nomaed@gmail.com>
2016-08-06 15:32:39 +0300

    setting git prompt's count of untracked files to true due to update in fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -45,7 +45,7 @@ function fish_prompt
   set -g __fish_git_prompt_color_prefix white
   set -g __fish_git_prompt_color_suffix white
   set -g __fish_git_prompt_showdirtystate true
-  set -g __fish_git_prompt_showuntrackedfiles false
+  set -g __fish_git_prompt_showuntrackedfiles true
   set -g __fish_git_prompt_showstashstate true
   set -g __fish_git_prompt_show_informative_status true
 
